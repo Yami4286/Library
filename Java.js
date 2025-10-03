c934c4b85add105310a092ca279c133986745acf
Yami4286 <abdulhanan8974@gmail.com>
2025-10-03 19:34:01 +0500

    so js is finally ready some falws but ok styling tomorrow

--- Java.js
+++ Java.js
@@ -1,18 +1,66 @@
 let Lib = [];
-let selec = document.getElementById("add");
 
-let btn = document.querySelector("btn");
+let form = document.querySelector("#bookform");
 
-btn.addEventListener("click", ()=> { AdToLib(selec)});
+let get = document.querySelectorAll(".add");
+
+let scr = document.querySelector(".screen");
+
+form.addEventListener("submit", (e)=> {
+
+    e.preventDefault();
+    
+     
+    const values = [];
+    get.forEach(gets => values.push(gets.value));
+
+    if(values.some(v=> v.trim() === "")){
+        alert("Please fill out all fields");
+        return;
+    }
+
+    AdToLib(...values);
+
+
+
+});
 
 
 function Books(Name, Author , published , id){
  this.Name = Name;
  this.Author = Author;
  this.published = published;
+ this.id = id;
 }
 
 function AdToLib(Name, Author , published){
 const NewBook = new Books(Name, Author, published, crypto.randomUUID());
 Lib.push(NewBook);
+
+
+    let CreBook = document.createElement("div");
+
+let Ti = document.createElement("div");
+let Au = document.createElement("div");
+let Pu = document.createElement("div");
+let IDA = document.createElement("div");
+
+Ti.textContent = `Title:${NewBook.Name}`;
+Au.textContent = `Author:${NewBook.Author}`;
+Pu.textContent = `Published:${NewBook.published}`;
+IDA.textContent = `ID:${NewBook.id}`;
+
+CreBook.appendChild(Ti);
+CreBook.appendChild(Au);
+CreBook.appendChild(Pu);
+CreBook.appendChild(IDA);
+
+scr.appendChild(CreBook);
+
+get.forEach(input => input.value = '');
+
 }
+
+
+
+
